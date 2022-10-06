2305d3ada7da048ac6a073f232caadd5a81fd207
Thyrso <thyrso.kof@gmail.com>
2022-10-06 14:45:20 -0300

    feat: Inicia Menu lateral "Finalizando" a estrutura inicial das paginas. PS> Chars alterado para Agents

---
+++ src/pages/Agents.jsx
@@ -0,0 +1,14 @@
+import Header from '../components/Header';
+import SideMenu from '../components/SideMenu';
+
+function Agents () {
+  return(
+    <div>
+      <Header />
+      <SideMenu />
+      <h1>Agents PAGE</h1>
+    </div>
+  )
+}
+
+export default Agents;
