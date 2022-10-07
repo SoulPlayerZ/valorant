b509ebe7cc07e407dc7bd91bfc0efcdbf756196e
Thyrso <thyrso.kof@gmail.com>
2022-10-07 14:49:34 -0300

    feat: Estiliza Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,23 @@
+import React from "react";
+import valorantLogo from '../images/valorantLogo.png';
+import { FaTwitch, FaInstagram, FaGithub } from 'react-icons/fa';
+
 function Header () {
   return (
     <header>
-      <h3>Valorant Logo</h3>
+      <section  className="logo-container">
+        <img src={ valorantLogo } alt="valorant-logo" className="logo"/>
+      </section>
       <nav>
-        <a href="https://github.com/SoulPlayerZ" target="_blank" rel="noreferrer">github</a>
-        <a href="https://www.instagram.com/thyrsokof/?theme=dark" target="_blank" rel="noreferrer">Insta</a>
-        <a href="https://www.twitch.tv/soulplayer_z" target="_blank" rel="noreferrer">Twitch</a>
+        <a href="https://github.com/SoulPlayerZ" target="_blank" rel="noreferrer" >
+          <FaGithub className="social" />
+        </a>
+        <a href="https://www.instagram.com/thyrsokof/?theme=dark" target="_blank" rel="noreferrer">
+          <FaInstagram className="social"/>
+        </a>
+        <a href="https://www.twitch.tv/soulplayer_z" target="_blank" rel="noreferrer">
+          <FaTwitch className="social"/>
+        </a>
       </nav>
     </header>
   )
