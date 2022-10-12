1e16d318a10b474fcb05bcd778770f1240f8ca2e
Thyrso <thyrso.kof@gmail.com>
2022-10-12 12:36:39 -0300

    refactor: Transforma o card dos buddies em link.(Talvez nao vá ser usado)

--- src/components/GenericCard.jsx
+++ src/components/GenericCard.jsx
@@ -1,14 +1,15 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function GenericCard ({ content }) {
 
   return (
-    <section className="card generic-card">
+    <Link to={`/buddies/${content.uuid}`} className="card card-agent">
       <img src={ content.displayIcon } alt="agent-icon" className="card-img-top" />
       <section className="card-body">
         <h5 className="card-title">{ content.displayName }</h5>
       </section>
-    </section>
+    </Link>
   )
 }
 
